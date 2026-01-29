43ee0171bbcede6ad515e5159d0e24e0de9b2aae
antbaena <antoniocanetebaena1234@gmail.com>
2026-01-29 09:47:16 +0100

    Activate AstraCamera on bringup to take into account depth data in the scans

--- sancho_ws/src/sancho_bringup/launch/sancho_bringup.launch.py
+++ sancho_ws/src/sancho_bringup/launch/sancho_bringup.launch.py
@@ -21,25 +21,28 @@ def generate_launch_description():
     hokuyo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('urg_node2'), 'launch', 'urg_node2_2lidar.launch.py')
-        )
+        ),
+        launch_arguments={
+            'range_min': '0.05', # Filtra cualquier cosa a menos de 5cm del sensor
+        }.items()
     )
 
     # Lanzar cámara Astra
-    # astra_camera_launch = GroupAction([
-    #     PushRosNamespace('astra_camera'),
-    #     IncludeLaunchDescription(
-    #         XMLLaunchDescriptionSource(
-    #             os.path.join(get_package_share_directory('astra_camera'), 'launch', 'astra.launch.xml')
-    #         ),
-    #         launch_arguments={
-    #             'serial_number': "'20070830098'",
-    #             'camera_name': 'camera',
-    #             'color_fps': '15',
-    #             'depth_fps': '15',
-    #             'ir_fps': '15'
-    #         }.items()
-    #     )
-    # ])
+    astra_camera_launch = GroupAction([
+        PushRosNamespace('astra_camera'),
+        IncludeLaunchDescription(
+            XMLLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('astra_camera'), 'launch', 'astra.launch.xml')
+            ),
+            launch_arguments={
+                'serial_number': "'20070830098'",
+                'camera_name': 'camera',
+                'color_fps': '15',
+                'depth_fps': '15',
+                'ir_fps': '15'
+            }.items()
+        )
+    ])
 
     # Base móvil Ranger
     ranger_launch = IncludeLaunchDescription(
@@ -127,7 +130,7 @@ def generate_launch_description():
         # Base móvil y sensores
         ranger_launch,
         hokuyo_launch,
-        #astra_camera_launch,
+        astra_camera_launch,
         scan_merger_node,
         usb_cam_node,
         image_proc_node
